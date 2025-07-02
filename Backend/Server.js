196208e556c44b4d6b882f1331f7cb6d7a46f738
Nikhilesh <nikhileshgowda10@gmail.com>
2025-07-02 12:32:08 -0700

    commit2

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,14 +3,14 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3900;
 
 // PostgreSQL connection
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   database: 'Leave',
-  password: 'Veera@0134',
+  password: 'admin123',
   port: 5432,
 });
 
